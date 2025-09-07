734e8e8063a15dc03d157e6e109b78eaa85cd2c7
Asgardijan <jan@mfrischknecht.ch>
2025-09-07 22:10:23 +0200

    fixed links

--- script.js
+++ script.js
@@ -2,6 +2,13 @@ const button = document.getElementById("submit");
 
 button.addEventListener("click", () => {
 
-  alert("Konnte nicht gesendet werden!");
+  const nameVar = document.getElementById("name").value;
+  const mailVar = document.getElementById("email").value;
+  const msgVar = document.getElementById("nachricht").value;
 
+  console.log("Name: " + nameVar);
+  console.log("E-Mail: " + mailVar);
+  console.log("Message: " + msgVar);
+
+  alert("Konnte nicht gesendet werden!");
 })
